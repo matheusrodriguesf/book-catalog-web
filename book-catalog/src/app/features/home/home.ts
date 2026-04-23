e0608a2f7b04c16e9a83c10557e1f92b4a3a9d76
Matheus Rodrigues <matheus.arcelino@mds.gov.br>
2026-04-23 15:15:52 -0300

    Menu && Rodape

--- book-catalog/src/app/features/home/home.ts
+++ book-catalog/src/app/features/home/home.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.html',
   styleUrl: './home.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Home {}
